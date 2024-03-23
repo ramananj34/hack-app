aa98edfd903144f9f70a15de5d36d28019d973ec
Jude <juder.ramanan@gmail.com>
2024-03-23 12:48:49 -0400

    do more tests

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-yellow-100 text-gray">
-      <div className="absolute left-0 top-0 h-16 w-16 bg-red">
+    <div className="h-screen w-screen bg-yellow-100">
+      <div className="absolute left-0 top-0 h-16 w-16 bg-red-50">
         Hello
       </div>
     </div>
