bb329faed573d4b0cbbe51a5258a96599c6d6e87
Jude <juder.ramanan@gmail.com>
2024-03-23 13:37:47 -0400

    Worked on header

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-red-0">
-      <div className="flex left-0 top-0 h-32 w-screen bg-red-800 justify-center place-content-center">
-        <h1 className="">Title</h1>
+    <div className="h-screen w-screen bg-white">
+      <div className="flex left-0 top-0 h-32 w-screen bg-red-800 justify-center items-center">
+        <h1 className="text-center text-white text-4xl">Title</h1>
       </div>
     </div>
   );
