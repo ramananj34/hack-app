7213c120aa448090eb0ac114cd0c651d263649d2
Jude <juder.ramanan@gmail.com>
2024-03-23 12:51:27 -0400

    Red

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-yellow-100">
-      <div className="absolute left-0 top-0 h-16 w-16 bg-red-50">
-        Hello
+      <div className="absolute left-0 top-0 h-16 w-16 bg-red-300">
       </div>
     </div>
   );
