78465afaeebc70f1f5952fb4bfaaabe7639f8c73
Alexander Radovich <alex@alexandersmbp3.colgate.edu>
2024-03-23 18:39:00 -0400

    style changes

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,8 +4,8 @@ import ExpandableButtonAdd from "../../components/Expandable-Button-Add.js";
 export default function Page() {
     return(
       <div className="h-screen w-screen bg-white justify-center items-center">
-      <div className="flex left-0 top-0 h-32 w-screen bg-red-800 justify-center items-center">
-      <h1 className="text-center text-white text-4xl">Colgate Surveys Admin</h1>
+      <div className="drop-shadow-md flex left-0 top-0 h-32 w-screen bg-red-800 justify-center items-center">
+      <h1 className="font-serif text-center text-white text-4xl">Colgate Surveys Admin</h1>
       </div>
       <br/>
       <ExpandableButtonAdd />
