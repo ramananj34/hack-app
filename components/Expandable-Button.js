963c9572f9c52e0466e5a902728bb36932d98cb6
Jude <juder.ramanan@gmail.com>
2024-03-23 15:07:12 -0400

    Make them able to fill out

--- components/Expandable-Button.js
+++ components/Expandable-Button.js
@@ -9,13 +9,17 @@ export default function ExpandableButton() {
     setExpanded(!expanded);
   };
 
+  const handleInnerClick = (event) => {
+    event.stopPropagation();
+  };
+
   return (
 
     <div className="bg-slate-300 w-11/12 p-4 pl-5 pr-5 rounded-lg shadow cursor-pointer mx-auto" onClick={toggleExpanded}>
         <h2 class="text-black font-bold mb-2">What do you feel about cutting down the trees in the middle campus?</h2>
         
         {expanded && ( 
-            <div className="bg-slate-400 p-4 pl-5 pr-5 rounded-lg">
+            <div className="bg-slate-400 p-4 pl-5 pr-5 rounded-lg" onClick={handleInnerClick}>
                 <form>
                     <label for="option1">
                         <input type="checkbox" id="option1" name="option" value="option1" />
