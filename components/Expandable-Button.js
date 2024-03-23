b211bc89c3b26876dc30ae7cbcc6a649fbf7644e
Jude <juder.ramanan@gmail.com>
2024-03-23 14:57:30 -0400

    small change to button

--- components/Expandable-Button.js
+++ components/Expandable-Button.js
@@ -11,29 +11,29 @@ export default function ExpandableButton() {
 
   return (
 
-    <div className="bg-slate-300 w-11/12 p-4 rounded-lg shadow cursor-pointer mx-auto" onClick={toggleExpanded}>
+    <div className="bg-slate-300 w-11/12 p-4 pl-5 pr-5 rounded-lg shadow cursor-pointer mx-auto" onClick={toggleExpanded}>
         <h2 class="text-black font-bold mb-2">What do you feel about cutting down the trees in the middle campus?</h2>
         
         {expanded && ( 
-            <form>
-                <label for="option1">
-                    <input type="checkbox" id="option1" name="option" value="option1" />
-                    I think it is a good idea!
-                </label>
-                <br/>
-  
-                <label for="option2">
-                    <input type="checkbox" id="option2" name="option" value="option2" />
-                    I think it is a bad idea.
-                </label>
-                <br/>
-                <p>Colgate Email: </p>
-                <br/>
-                <input />
-                <br />
-  
-                <input type="submit" value="Submit" />
-            </form>
+            <div className="bg-slate-400 p-4 pl-5 pr-5 rounded-lg">
+                <form>
+                    <label for="option1">
+                        <input type="checkbox" id="option1" name="option" value="option1" />
+                        I think it is a good idea!
+                    </label>
+                    <br/>
+    
+                    <label for="option2">
+                        <input type="checkbox" id="option2" name="option" value="option2" />
+                        I think it is a bad idea.
+                    </label>
+                    <br/>
+                    <p>Colgate Email:   <input /></p>
+                    <br />
+    
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         )}
       </div>
   );
