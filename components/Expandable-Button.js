cf8cd3b7e5dd87618fd14f6fa974d7c4b3933019
Jude <juder.ramanan@gmail.com>
2024-03-23 15:44:14 -0400

    fix submit button

--- components/Expandable-Button.js
+++ components/Expandable-Button.js
@@ -17,29 +17,34 @@ export default function ExpandableButton() {
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
+
   return (
 
     <div className="bg-slate-300 w-11/12 p-4 pl-5 pr-5 rounded-lg shadow cursor-pointer mx-auto" onClick={toggleExpanded}>
         <h2 class="text-black font-bold mb-2">What do you feel about cutting down the trees in the middle campus?</h2>
         
         {expanded && ( 
-            <div className="bg-slate-400 p-4 pl-5 pr-5 rounded-lg" onClick={handleInnerClick}>
+            <div className="relative bg-slate-400 p-4 pl-5 pr-5 rounded-lg" onClick={handleInnerClick}>
                 <form>
                     <label for="option1">
-                        <input type="checkbox" id="option1" name="option" value="option1" checked={selectedOption === "option1"} onChange={handleOptionChange}/>
+                        <input type="radio" id="option1" name="option" value="option1" className="accent-red-800" checked={selectedOption === "option1"} onChange={handleOptionChange}/>
                         <p className="inline-flex">&nbsp;I think it is a good idea!</p>
                     </label>
                     <br/>
     
                     <label for="option2">
-                        <input type="checkbox" id="option2" name="option" value="option2" checked={selectedOption === "option2"} onChange={handleOptionChange}/>
+                        <input type="radio" id="option2" name="option" value="option2" className="accent-red-800" checked={selectedOption === "option2"} onChange={handleOptionChange}/>
                         <p className="inline-flex">&nbsp;I think it is a bad idea.</p>
                     </label>
+
+                    <br/>
                     <br/>
                     <p>Colgate Email:&nbsp;<input /></p>
                     <br />
     
-                    <input type="submit" value="Submit" />
+                    <div className="absolute w-min p-2 rounded-lg bg-red-800 bottom-2 right-2">
+                        <p className="text-white">Submit</p>
+                    </div>
                 </form>
             </div>
         )}
