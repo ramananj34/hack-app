cc78ab20093ea9d3f9cc37400378f58d0ebde676
Jude <juder.ramanan@gmail.com>
2024-03-23 13:26:36 -0400

    Start header

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-yellow-100">
-      <div className="absolute left-0 top-0 h-16 w-16 bg-red-300">
+    <div className="h-screen w-screen bg-red-0">
+      <div className="flex left-0 top-0 h-32 w-screen bg-red-800 justify-center place-content-center">
+        <h1 className="">Title</h1>
       </div>
-      <hr/>
     </div>
   );
 }
