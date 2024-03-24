de99b1b3e51388425ac6d628f6726a32479f8f72
Hugo Stickney <hugostickney@gmail.com>
2024-03-23 22:46:42 -0400

    Adminback

--- app/admin/page.js
+++ app/admin/page.js
@@ -26,6 +26,11 @@ const clickLogin = () => {
       <div className="h-screen w-screen bg-white justify-center items-center">
       <div className="drop-shadow-md flex left-0 top-0 h-32 w-screen bg-red-800 justify-center items-center">
       <h1 className="font-serif text-center text-white text-4xl">Colgate Surveys Admin</h1>
+      <a href = "/">
+        <div className="absolute w-min p-2 rounded-lg bg-slate-400 top-4 right-4 cursor-pointer">
+          <p className="text-white">Back</p>
+        </div>
+      </a>
       </div>
       <br/>
 
