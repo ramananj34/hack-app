9087dab565cb31f57660f4c9da446e238933c406
Jude <juder.ramanan@gmail.com>
2024-03-23 22:58:19 -0400

    start fetch

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import React from "react";
+'use client'
+
 import ExpandableButton from "../components/Expandable-Button";
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
   // QUERY THE DATABASE FOR ALL THE QUESTIONS
-  
+
+    const [data, setData] = useState({});
+    const [qName, setQName] = useState("");
+    const [dataObject, setDataObject] = useState();
+
+    useEffect(() => {
+      fetch('http://localhost:3000/api/questions/')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+    }, []);
+
 
   return (
     <div className="h-screen w-screen bg-white justify-center items-center">
@@ -18,6 +31,15 @@ export default function Home() {
       <br/>
       <br/>
       <br/>
+      {
+        // (JSON.parse(JSON.stringify(data))).entries().forEach((element) => {
+        //   <div>{element.QuestionName}</div>
+        // }) 
+        
+        Object.entries(JSON.parse(JSON.stringify(data))).forEach((entry) => {
+          <ExpandableButton question="What do you feel about cutting down the trees in the middle campus?" answers={['I think it is a good idea!', 'I think it is a bad idea.']} />
+        })
+      }
       <ExpandableButton question="What do you feel about cutting down the trees in the middle campus?" answers={['I think it is a good idea!', 'I think it is a bad idea.']} />
       <br/>
       <br/>
