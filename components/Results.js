b63d288734ccb38e6c586dccb7e179102c3bc166
Hugo Stickney <hugostickney@gmail.com>
2024-03-24 02:02:58 -0400

    Delete

--- components/Results.js
+++ components/Results.js
@@ -1,10 +1,13 @@
 'use client'
 
 
+
 import React, { useState } from 'react';
 
 export default function Results({ question, answers }) {
-
+    const clickDelete = () => {
+        console.log("heyyyy");
+    };
     return(
         <div className="bg-slate-300 w-11/12 p-4 pl-5 pr-5 rounded-lg shadow mx-auto" >
             <h2 className="text-black font-bold mb-2">{question}</h2>
@@ -13,7 +16,13 @@ export default function Results({ question, answers }) {
                     <li key={index}>{answer[0]} : {answer[1].length}</li>
                 ))}
             </ul>
+            <div className="relative flex justify-end items-center">
+                <div className="w-min p-2 rounded-lg bg-red-800 cursor-pointer" onClick={clickDelete}>
+                    <p className="text-white">Delete</p>
+                </div>
+            </div>
         </div>
+
 
         
     );
