8f999b5b7e1814f5697b4e9628b3399d8d539f77
Jude <juder.ramanan@gmail.com>
2024-03-24 01:16:37 -0400

    Cant click on display

--- components/Results.js
+++ components/Results.js
@@ -6,7 +6,7 @@ import React, { useState } from 'react';
 export default function Results({ question, answers }) {
 
     return(
-        <div className="bg-slate-300 w-11/12 p-4 pl-5 pr-5 rounded-lg shadow cursor-pointer mx-auto" >
+        <div className="bg-slate-300 w-11/12 p-4 pl-5 pr-5 rounded-lg shadow mx-auto" >
             <h2 className="text-black font-bold mb-2">{question}</h2>
             <ul>
                 {answers.map((answer, index) => (
