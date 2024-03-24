56381fc3bd10337e25e6d5b78702715a13d0cfe9
Jude <juder.ramanan@gmail.com>
2024-03-24 03:59:39 -0400

    revert change

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
 
 
   return (
-    <div key={person.id} className="h-screen w-screen bg-white justify-center items-center">
+    <div className="h-screen w-screen bg-white justify-center items-center">
       <div className="drop-shadow-md flex left-0 top-0 h-32 w-screen bg-red-800 justify-center items-center">
         <h1 className="font-serif text-center text-white text-4xl">Colgate Surveys</h1>
       </div>
