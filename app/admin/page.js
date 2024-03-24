dd365ee37a0a5a9fe0e2dad566a664dc1ed425e9
Hugo Stickney <hugostickney@gmail.com>
2024-03-23 22:10:36 -0400

    Admin PW

--- app/admin/page.js
+++ app/admin/page.js
@@ -16,6 +16,11 @@ const handleUsernameChange = (event) => {
 const handlePasswordChange = (event) => {
   setPasswordInput(event.target.value);
 };
+const clickLogin = () => {
+  if (usernameInput == "admin" && passwordInput== "admin") {
+    setIsAdmin(true);
+  }
+};
   
   return(
       <div className="h-screen w-screen bg-white justify-center items-center">
@@ -25,11 +30,18 @@ const handlePasswordChange = (event) => {
       <br/>
 
       {!isAdmin && (
-        <div className="relative bg-slate-400 p-4 pl-5 pr-5 rounded-lg cursor-default">
-          <form>
-            <p>CUsername:&nbsp;<input id="username" onChange={handleUsernameChange}/></p>
-            <p>Password:&nbsp;<input id="password" onChange={handlePasswordChange}/></p>
-          </form>
+        <div className="flex justify-center">
+          <div className="relative bg-slate-400 w-3/6 p-4 pl-5 pr-5 rounded-lg cursor-default items-center">
+            <form>
+              <p>Username:&nbsp;<input id="username" onChange={handleUsernameChange}/></p>
+              <br/>
+              <p>Password:&nbsp;<input type="password" id="password" onChange={handlePasswordChange}/></p>
+            </form>
+            
+              <div className="absolute w-min p-2 top-8 right-8 rounded-lg bg-red-800 cursor-pointer" onClick={clickLogin}>
+                <p className="text-white">Login</p>
+              </div>
+          </div>
         </div>
       )}
 
